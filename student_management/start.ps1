2ffc4c2dc13206e870f98bb54d13baa683048d74
unknown <m.rajsingh1301@gmail.com>
2024-11-30 17:05:47 +0530

    adjust 2

--- student_management/start.ps1
+++ student_management/start.ps1
@@ -1,1 +1,10 @@
-uvicorn main:app --host=localhost --port=8000
+
+if ($IsWindows) {
+    Write-Host "Running on Windows"
+    
+    Start-Process -NoNewWindow -FilePath "uvicorn" -ArgumentList "main:app", "--host", "localhost", "--port", "8000"
+} else {
+    Write-Host "Running on Linux"
+    
+    uvicorn main:app --host localhost --port 8000
+}
